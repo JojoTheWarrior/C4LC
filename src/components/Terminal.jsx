5d74a0a34e71484c3aebf7fa3c14ad8c49615868
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-09-21 13:01:00 -0400

    array of strings for terminal

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -1,48 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Terminal.css';
+import Typed from "typed.js";
 
 const Terminal = () => {
   const [input, setInput] = useState('');
   const [submitted, setSubmitted] = useState(false);
 
-  const handleInputChange = (e) => {
-    const value = e.target.value;
-    // Only allow numbers and limit input to 9 digits
-    if (/^\d*$/.test(value) && value.length <= 9) {
-      setInput(value);
-    }
-  };
+  /* array of elements representing the terminal */
+  const [terminal, setTerminal] = useState([]);
 
-  // form takes in 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (input.length === 9) {
-      setSubmitted(true);
-      console.log("Submitted Password: ", input);
+  /* displaying solution */
+  const [solutions, setSolutions] = useState([]);
+  const [showTypedOutput, setShowTypedOutput] = useState(false);
+
+  useEffect(() => {
+    if (showTypedOutput) {
+      
+      const options = {
+        strings: solutions, // array of strings for each step in the solution
+        typeSpeed: 50,      
+        backSpeed: 25,      
+        showCursor: true,   // display cursor
+        cursorChar: "|",    // customize cursor character
+        loop: false         
+      };
+
+      // initialize typed.js on the element
+      // const typed = new Typed(typedElement.current, options);
+
+      // cleanup typed.js when the component unmounts
+      return () => {
+        // typed.destroy();
+      };
     }
-  };
+  }, [showTypedOutput, solutions]);
 
   return (
-    <div className="terminal-container">
-      <form onSubmit={handleSubmit}>
-        <div className="terminal">
-
-            {/* Input 9-digit student number */}
-            <span className="terminal-prompt">$ </span>
-            <input
-                type="text"
-                className="terminal-input"
-                value={input}
-                onChange={handleInputChange}
-                placeholder="Enter 9-digit code"
-                autoFocus
-            />
-        </div>
-        <button type="submit" className="submit-btn">Submit</button>
-      </form>
-
-      {submitted && <p>Password Submitted: {input}</p>}
-    </div>
+    <></>
   );
 };
 
