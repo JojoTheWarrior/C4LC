f89b8831bf69f0f883302b8da6f4fed1444b6951
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-09-21 18:01:09 -0400

    added reference for typed element

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -6,11 +6,29 @@ const Terminal = () => {
   const [input, setInput] = useState('');
   const [submitted, setSubmitted] = useState(false);
 
+  const typedElementRef = useRef(null); // current line being typed
+  const typedInstanceRef = useRef(null); // typed.js instance
+
+
   /* array of elements representing the terminal */
   const [terminal, setTerminal] = useState([
     <p>Welcome to C4LC, a program to check your work for the Vectors Culminating Activity! </p>,
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
   ]);
+
+  const addNewLine = (newLineText) => {
+    const newLine = {
+        text: newLineText,
+        key: terminal.length,
+    }
+    setTerminal((prevLines) => [...prevLines, newLine]);
+  }
+
+  const startTyping = (newText) => {
+    if (typedInstanceRef.current) typedInstanceRef.current.destroy();
+
+    // initialize typed.js
+  }
 
   return (
     <div className="terminal-container">
